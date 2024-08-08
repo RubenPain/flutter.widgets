aefc4c8edd7e938f8ce5af131084ad8a3cd7dcbb
Ruben Pain <52694768+RubenPain@users.noreply.github.com>
2024-08-08 10:26:58 +0200

    Update scrollable_positioned_list.dart

--- packages/scrollable_positioned_list/lib/src/scrollable_positioned_list.dart
+++ packages/scrollable_positioned_list/lib/src/scrollable_positioned_list.dart
@@ -421,7 +421,7 @@ class _ScrollablePositionedListState extends State<ScrollablePositionedList>
       if (_lastTargetKey != null) {
         var currTargetIndex = _getIndexOfKey();
         if (currTargetIndex != null && currTargetIndex > primary.target) {
-          primary.target = currTargetIndex;
+          setState(() {primary.target = currTargetIndex;});
         }
       }
     }
